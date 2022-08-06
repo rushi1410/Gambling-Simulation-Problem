5f3df16e00526fb55424100d12b73c4519115db6
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-06 23:45:09 +0530

    Added:Welcome To Gambling Simulation Problem

---
+++ src/com/bridgelabz/GamblingProblem/GamblingSimulation.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.GamblingProblem;
+
+public class GamblingSimulation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Gambling Simulation Problem");
+	}
+}
