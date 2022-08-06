6658dac1e20f32170aac6ac309129e3e2b8e124a
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-06 23:52:15 +0530

    Modified:Initialize Variables

--- src/com/bridgelabz/GamblingProblem/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingProblem/GamblingSimulation.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.GamblingProblem;
 
 public class GamblingSimulation {
-
+    static int DAILY_STAKE = 100;
+    static int PER_DAY_BET = 1;
+    
 	public static void main(String[] args) {
 		System.out.println("Welcome To Gambling Simulation Problem");
 	}
