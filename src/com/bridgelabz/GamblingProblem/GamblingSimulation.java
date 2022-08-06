fa991f6409086409d86eb9f70b1a6ca870602e54
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-07 00:11:38 +0530

    Modified:As a Gambler make  bet So Check Win Or Loss

--- src/com/bridgelabz/GamblingProblem/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingProblem/GamblingSimulation.java
@@ -1,10 +1,23 @@
 package com.bridgelabz.GamblingProblem;
 
 public class GamblingSimulation {
+
     static int DAILY_STAKE = 100;
     static int PER_DAY_BET = 1;
-    
+    static int win = 1;
 	public static void main(String[] args) {
 		System.out.println("Welcome To Gambling Simulation Problem");
+		  int result = (int)(Math.random()*10)%2;
+	        if (result==win){
+	            DAILY_STAKE++;
+	            System.out.println("Won");
+	        }
+	        else{
+	            DAILY_STAKE--;
+	            System.out.println("lost");
+	        }
+	        System.out.println(DAILY_STAKE );
+
+	    }
 	}
-}
+
