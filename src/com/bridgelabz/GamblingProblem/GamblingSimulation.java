64fa28e320778de15e03739f87b6eb70e073869f
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-07 00:25:54 +0530

    Modified:Condition For 50 % Of Win Or Loss Stake

--- src/com/bridgelabz/GamblingProblem/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingProblem/GamblingSimulation.java
@@ -2,22 +2,24 @@ package com.bridgelabz.GamblingProblem;
 
 public class GamblingSimulation {
 
-    static int DAILY_STAKE = 100;
-    static int PER_DAY_BET = 1;
-    static int win = 1;
+	static int DAILY_STAKE = 100;
+	static int PER_DAY_BET = 1;
+	static int win = 1;
 	public static void main(String[] args) {
 		System.out.println("Welcome To Gambling Simulation Problem");
-		  int result = (int)(Math.random()*10)%2;
-	        if (result==win){
-	            DAILY_STAKE++;
-	            System.out.println("Won");
-	        }
-	        else{
-	            DAILY_STAKE--;
-	            System.out.println("lost");
-	        }
-	        System.out.println(DAILY_STAKE );
+		while(DAILY_STAKE<150 && DAILY_STAKE>50){
+			int result = (int)(Math.random()*10)%2;
+			if (result==win){
+				DAILY_STAKE++;
+				System.out.println("Won");
+			}
+			else{
+				DAILY_STAKE--;
+				System.out.println("lost");
+			}
+		}
+		System.out.println(DAILY_STAKE  );
 
-	    }
 	}
+}
 
