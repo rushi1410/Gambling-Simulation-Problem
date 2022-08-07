d707530df77377604963347378112d790ae4bd66
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-07 18:38:09 +0530

    Modified:If Won Would Like To Continue Playing Next Month Or Stop Gambling

--- src/com/bridgelabz/GamblingProblem/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingProblem/GamblingSimulation.java
@@ -1,9 +1,10 @@
 package com.bridgelabz.GamblingProblem;
 
+import java.util.Scanner;
 
 public class GamblingSimulation {
 
-	static int STAKE = 100;
+	static final int STAKE = 100;
 	static int PER_DAY_BET = 1;
 	static int win = 1;
 	static int daysWon=0;
@@ -18,11 +19,10 @@ public class GamblingSimulation {
 	static int lostMax=0;
 	static int luckyDay;
 	static int unluckyDay;
+	static int month =1;
 
 
-
-	public static void main(String[] args) {
-		System.out.println("Welcome To Gambling Simulation Problem");
+	public static void game(){
 		for (int day =1; day<=DAYS_IN_MONTH;day++) {
 			dailyStake=STAKE;
 			while (dailyStake < MAX_WIN_PER_DAY && dailyStake > MAX_LOSS_PER_DAY) {
@@ -53,16 +53,41 @@ public class GamblingSimulation {
 				System.out.println("Lost on day "+day +" is "+ (STAKE-dailyStake));
 				daysLost++;
 			}
-
 		}
+	}
+	static void continueGame(){
+		while(true) {
+			System.out.println("Total amount won in 20 days is "+ totalAmountWonFor20Day);
+			System.out.println("Total amount lost in 20 days is "+ totalAmountLostFor20Day);
+			System.out.println("No of days won is: "+ daysWon);
+			System.out.println("No of days Lost is: "+ daysLost);
+			System.out.println("luckiest day is day " +luckyDay);
+			System.out.println("Unlukiest day is day " +unluckyDay);
+			int totalProfitLoss = totalAmountWonFor20Day + totalAmountLostFor20Day;
+			System.out.println("Total Profit or loss in month "+month +" is "+ totalProfitLoss);
+			month++;
+			if (totalProfitLoss >= 100) {
+				System.out.println("Eligible to continue for next month");
+				System.out.println("Do you want to continue(Y/y)");
+				Scanner scr = new Scanner(System.in);
+				char option = scr.next().charAt(0);
+				if (option == 'y' || option == 'Y') {
+					game();
+				}
+				else{
+					break;
+				}
+			} else {
+				System.out.println("You are in Loss!!!Not Eligible to continue");
+				break;
+			}
+		}
+	}
 
-		System.out.println("Total amount won in 20 days is "+ totalAmountWonFor20Day);
-		System.out.println("Total amount lost in 20 days is "+ totalAmountLostFor20Day);
-		System.out.println("No of days won is: "+ daysWon);
-		System.out.println("No of days Lost is: "+ daysLost);
-		System.out.println("luckiest day is day " +luckyDay);
-		System.out.println("Unlukiest day is day " +unluckyDay);
-
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Simulator");
+		game();
+		continueGame();
 	}
 }
 
